731ece0f699fce248036b357f8532c218b4668cb
André Gomes <agomes@altyra.com>
2021-03-13 12:26:37 +0000

    teste

--- src/adm_Classes/Main02.java
+++ src/adm_Classes/Main02.java
@@ -7,7 +7,6 @@ import Model.Pessoa;
 public class Main02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<Pessoa> lista = new ArrayList<Pessoa>();
 		lista.add(new Pessoa("André", "Gomes", "Lisboa", 186, 120));
 		lista.add(new Pessoa("Inês", "Gomes", "Lisboa", 186, 120));
